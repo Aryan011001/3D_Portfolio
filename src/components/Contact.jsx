d310d4a80a5f6ebab204107c1df3fdfd095ee91a
Aryan Ranjan <aryanranjan@Aryans-MacBook-Air.local>
2023-07-15 02:27:05 +0530

    few tweaks

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -83,4 +83,4 @@ const Contact = () => {
   )
 }
 
-export default SectionWrapper(Contact, "")
+export default SectionWrapper(Contact, "contact")
--- src/constants/index.js
+++ src/constants/index.js
@@ -31,7 +31,7 @@ export const navLinks = [
         title: "About",
     },
     {
-        id: "work",
+        id: "experience",
         title: "Work",
     },
     {
